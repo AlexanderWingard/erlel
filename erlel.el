c34c368cf2cc101103aa7571b09bc7de5551c472
alexander.wingard@gmail.com <alexander.wingard@ericsson.com>
2017-10-03 17:00:20 +0200

    Start working on stuff
    Change-Id: I38c0317a84f7f1c0e39aec5f1579141753e39f42

--- erlel.el
+++ erlel.el
@@ -1,24 +1,25 @@
-(require 'erlang)
-(require 'projectile)
-(require 'helm)
+(dolist (dep '(erlang
+               projectile
+               helm))
+  (when (not (package-installed-p dep))
+    (package-install dep)
+    (require dep)))
 
 (defun erlel-grep-function (name module)
-  (split-string
-   (shell-command-to-string
-    (concat "git grep -n ^" name " -- *" module ".erl"))
-   "\n"))
+  (erlel-grep "git grep -n ^" name "\\( -- :/*" module ".erl"))
 
 (defun erlel-grep-macro (name)
-  (split-string
-   (shell-command-to-string
-    (concat "git grep -n define\\(" name " -- *.[eh]rl"))
-   "\n"))
+  (erlel-grep "git grep -n '\\-define(" name "[ ,(]' -- :/*.[eh]rl"))
 
 (defun erlel-grep-record (name)
+  (erlel-grep "git grep -n '\\-record(" name "[ ,]' -- :/*.[eh]rl"))
+
+(defun erlel-grep (&rest args)
   (split-string
    (shell-command-to-string
-    (concat "git grep -n record\\(" name " -- *.[eh]rl"))
-   "\n"))
+    (apply 'concat args))
+   "\n"
+   t))
 
 (defun erlel-dwim ()
   (interactive)
@@ -27,24 +28,23 @@
          (module (erlang-id-module id))
          (name (erlang-id-name id))
          (arity (erlang-id-arity id))
-         (grepper (cond ((and module name arity)
-                         (lambda () (erlel-grep-function name module)))
+         (matches (cond ((and module name arity)
+                         (erlel-grep-function name module))
                         ((and (eq kind 'macro))
-                         (lambda () (erlel-grep-macro name)))
+                         (erlel-grep-macro name))
                         ((and (eq kind 'record))
-                         (lambda () (erlel-grep-record name))))))
-    (if grepper
-        (projectile-with-default-dir (projectile-project-root)
-          (helm :sources
-                (helm-build-sync-source "Matches"
-                  :candidates grepper
-                  :action (lambda (s)
-                            (let* ((tokens (split-string s ":"))
-                                   (file (nth 0 tokens))
-                                   (line (nth 1 tokens)))
-                              (find-file file)
-                              (goto-char (point-min))
-                              (forward-line (1- (string-to-number line))))))))
+                         (erlel-grep-record name)))))
+    (if matches
+        (helm :sources
+              (helm-build-sync-source "Matches"
+                :candidates matches
+                :action (lambda (s)
+                          (let* ((tokens (split-string s ":"))
+                                 (file (nth 0 tokens))
+                                 (line (nth 1 tokens)))
+                            (find-file file)
+                            (goto-char (point-min))
+                            (forward-line (1- (string-to-number line)))))))
       (print id))))
 
 (global-set-key (kbd "M-ä") 'erlel-dwim)
