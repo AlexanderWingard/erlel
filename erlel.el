0b85cfc9992631526763dc341479e69fafdb105a
alexander.wingard@gmail.com <alexander.wingard@ericsson.com>
2017-10-05 15:57:32 +0200

    Refactor
    Change-Id: I9b03e8df50dc5eb646b4c95c9bf26b8ebf4f3940

--- erlel.el
+++ erlel.el
@@ -5,71 +5,39 @@
     (package-install dep)
     (require dep)))
 
-(defun erlel-grep-function (name module)
-  (erlel-grep "git grep -n ^" name "\\( -- :/*" module ".erl"))
+(defun erlel-find-uses ()
+  (interactive)
+  (erlel-find-thing 'uses))
 
-(defun erlel-grep-calls-function (name module)
-  (erlel-grep "git grep -n " module ":" name " -- :/*"))
-
-(defun erlel-grep-macro (name)
-  (erlel-grep "git grep -n '\\-define(" name "[ ,(]' -- :/*.[eh]rl"))
-
-(defun erlel-grep-calls-macro (name)
-  (erlel-grep "git grep -n \\?" name " -- :/*"))
-
-(defun erlel-grep-record (name)
-  (erlel-grep "git grep -n '\\-record(" name "[ ,]' -- :/*.[eh]rl"))
-
-(defun erlel-grep-calls-record (name)
-  (erlel-grep "git grep -n '#" name "' -- :/*"))
+(defun erlel-find-definitions ()
+  (interactive)
+  (erlel-find-thing 'definition))
 
 (defun erlel-grep (&rest args)
-  (split-string
-   (shell-command-to-string
-    (apply 'concat args))
-   "\n"
-   t))
+  (split-string (shell-command-to-string (apply 'concat args)) "\n" t))
 
-(defun erlel-who-calls ()
-  (interactive)
+(defun erlel-find-thing (what)
   (let* ((id (erlang-get-identifier-at-point))
          (kind (erlang-id-kind id))
          (module (erlang-id-module id))
          (name (erlang-id-name id))
-         (arity (erlang-id-arity id))
-         (matches (cond ((and module name arity)
-                         (erlel-grep-calls-function name module))
-                        ((and (eq kind 'macro))
-                         (erlel-grep-calls-macro name))
-                        ((and (eq kind 'record))
-                         (erlel-grep-calls-record name)))))
-    (if matches
-        (helm :sources
-              (helm-build-sync-source "Matches"
-                :candidates matches
-                :action (lambda (s)
-                          (let* ((tokens (split-string s ":"))
-                                 (file (nth 0 tokens))
-                                 (line (nth 1 tokens)))
-                            (find-file file)
-                            (goto-char (point-min))
-                            (forward-line (1- (string-to-number line)))
-                            (recenter-top-bottom)))))
-      (print id))))
+         (matches
+          (cond
+           ((eq what 'definition)
+            (cond ((and module name)
+                   (erlel-grep "git grep -n ^" name "\\( -- :/*" module ".erl"))
+                  ((and (eq kind 'macro))
+                   (erlel-grep "git grep -n '\\-define(" name "[ ,(]' -- :/*.[eh]rl"))
+                  ((and (eq kind 'record))
+                   (erlel-grep "git grep -n '\\-record(" name "[ ,]' -- :/*.[eh]rl"))))
 
-(defun erlel-dwim ()
-  (interactive)
-  (let* ((id (erlang-get-identifier-at-point))
-         (kind (erlang-id-kind id))
-         (module (erlang-id-module id))
-         (name (erlang-id-name id))
-         (arity (erlang-id-arity id))
-         (matches (cond ((and module name arity)
-                         (erlel-grep-function name module))
-                        ((and (eq kind 'macro))
-                         (erlel-grep-macro name))
-                        ((and (eq kind 'record))
-                         (erlel-grep-record name)))))
+           ((eq what 'uses)
+            (cond ((and module name)
+                   (erlel-grep "git grep -n " module ":" name " -- :/*"))
+                  ((and (eq kind 'macro))
+                   (erlel-grep "git grep -n \\?" name " -- :/*"))
+                  ((and (eq kind 'record))
+                   (erlel-grep "git grep -n '#" name "' -- :/*")))))))
     (if matches
         (helm :sources
               (helm-build-sync-source "Matches"
@@ -83,5 +51,5 @@
                             (forward-line (1- (string-to-number line)))))))
       (print id))))
 
-(global-set-key (kbd "M-ä") 'erlel-dwim)
-(global-set-key (kbd "M-å") 'erlel-who-calls)
+(global-set-key (kbd "M-ä") 'erlel-find-definitions)
+(global-set-key (kbd "M-å") 'erlel-find-uses)
